a97ea4cf35fd1dd49106961691ac2538d88f8b7c
Scott Sweazy Jr <86274124+sweazysm@users.noreply.github.com>
2021-10-17 17:33:07 -0400

    Updated Code

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -3,6 +3,9 @@ MechaCar_df = read.csv("MechaCar_mpg.csv", header=T,
                    colClasses = c("numeric", "numeric", "numeric", 
                                   "numeric", "numeric", "numeric"))
 # all colums are now numeric in value
+final.fit1=lm(vehicle_length~vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,
+            data=MechaCar_df)
+summary(final.fit1)
 
 # Final Code for Deliverable 1
 final.fit1=lm(vehicle_length~vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,
@@ -25,3 +28,19 @@ total_summary <- suspension_df %>% summarize(Mean = mean(PSI),
 
 lot_summary <- suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), 
 Median = median(PSI), Variance = var(PSI), SD = sqrt(var(PSI)), .groups = 'keep') #create summary table
+
+
+# DELIVERABLE 3 STARTS HERE
+
+# part 1
+t.test(suspension_df$PSI, mu=1500, alt='less')
+
+# part 2
+lot1_df <- subset(suspension_df, Manufacturing_Lot == 'Lot1')
+lot2_df <- subset(suspension_df, Manufacturing_Lot == 'Lot2')
+lot3_df <- subset(suspension_df, Manufacturing_Lot == 'Lot3')
+
+# part 3
+t.test(lot1_df$PSI, mu=1500, alt='less')
+t.test(lot2_df$PSI, mu=1500, alt='less')
+t.test(lot3_df$PSI, mu=1500, alt='less')
